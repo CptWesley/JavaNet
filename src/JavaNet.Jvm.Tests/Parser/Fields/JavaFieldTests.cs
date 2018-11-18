21dd63331735ec3c4a459baccf07326f06fc9407
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-11-18 09:58:28 +0100

    Add JavaField class

---
+++ src/JavaNet.Jvm.Tests/Parser/Fields/JavaFieldTests.cs
@@ -0,0 +1,32 @@
+﻿using JavaNet.Jvm.Parser.Attributes;
+using JavaNet.Jvm.Parser.Fields;
+using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace JavaNet.Jvm.Tests.Parser.Fields
+{
+    /// <summary>
+    /// Test class for the <see cref="JavaField" /> class
+    /// </summary>
+    public static class JavaFieldTests
+    {
+        /// <summary>
+        /// Constructors the test.
+        /// </summary>
+        [Fact]
+        public static void ConstructorTest()
+        {
+            JavaFieldAccessFlags flags = JavaFieldAccessFlags.Enum | JavaFieldAccessFlags.Private;
+            ushort us1 = 1;
+            ushort us2 = 2;
+            ushort us3 = 3;
+            IJavaAttribute[] attributes = new IJavaAttribute[2];
+            JavaField jf = new JavaField(flags, us1, us2, us3, attributes);
+            AssertThat(jf.AccessFlags).IsEqualTo(flags);
+            AssertThat(jf.NameIndex).IsEqualTo(us1);
+            AssertThat(jf.DescriptorIndex).IsEqualTo(us2);
+            AssertThat(jf.AttributesCount).IsEqualTo(us3);
+            AssertThat(jf.Attributes).ContainsExactly(attributes);
+        }
+    }
+}
---
+++ src/JavaNet.Jvm/Parser/Attributes/IJavaAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace JavaNet.Jvm.Parser.Attributes
+{
+    /// <summary>
+    /// Interface for java attributes.
+    /// </summary>
+    public interface IJavaAttribute
+    {
+        /// <summary>
+        /// Gets the index of the name in the constant pool.
+        /// </summary>
+        /// <value>
+        /// The index of the name in the constant pool.
+        /// </value>
+        ushort NameIndex { get; }
+
+        /// <summary>
+        /// Gets the length of the attribute in number of bytes.
+        /// </summary>
+        /// <value>
+        /// The length of the attribute in number of bytes.
+        /// </value>
+        uint Length { get; }
+    }
+}
