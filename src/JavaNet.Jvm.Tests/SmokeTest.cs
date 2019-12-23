1f246b4d5327f0431fd469606ca26e4ab31a1376
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2019-12-23 10:46:08 +0100

    Add smoke test

---
+++ src/JavaNet.Jvm.Tests/SmokeTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using CoreResourceManager;
+using JavaNet.Jvm.Interpreter;
+using JavaNet.Jvm.Parser;
+using JavaNet.Jvm.Parser.Attributes;
+using JavaNet.Jvm.Parser.Constants;
+using JavaNet.Jvm.Parser.Methods;
+using JavaNet.Jvm.Tests.Parser.Methods;
+using Xunit;
+using static AssertNet.Assertions;
+
+namespace JavaNet.Jvm.Tests
+{
+    /// <summary>
+    /// Contains smoke tests.
+    /// </summary>
+    public class SmokeTest
+    {
+        /// <summary>
+        /// Checks that we can parse and execute the hello world class correctly.
+        /// </summary>
+        [Fact]
+        public void HelloWorld()
+        {
+            using (Stream stream = Resource.Get("HelloWorld.class"))
+            {
+                JavaClass jc = JavaClass.Create(stream);
+                StringBuilder sb = new StringBuilder();
+                foreach (JavaMethod method in jc.Methods)
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"method {((JavaConstantUtf8)jc.ConstantPool[method.NameIndex]).Value}");
+                    JavaAttributeCode code = GetCode(method);
+                    foreach (byte b in code.Code)
+                    {
+                        sb.AppendLine(((JavaOpCode)b).ToString());
+                    }
+                }
+
+                throw new Exception(sb.ToString());
+            }
+        }
+
+        private JavaAttributeCode GetCode(JavaMethod method)
+        {
+            foreach (IJavaAttribute attribute in method.Attributes)
+            {
+                if (attribute is JavaAttributeCode code)
+                {
+                    return code;
+                }
+            }
+
+            throw new Exception("No code attribute found.");
+        }
+    }
+}
